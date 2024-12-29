6382171432ad977056aaa22566c4c5ee4fdcf89b
Aditya Shibu <as2397@hw.ac.uk>
2024-12-29 17:40:36 +0400

    add Population contract with People struct and functions for storing and retrieving favorite numbers

---
+++ Basic Solidity/People.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+// Define solidity version
+pragma solidity ^0.8.0;
+
+// Define contract
+contract Population {
+
+    // Define integer variable
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+    }
+}
