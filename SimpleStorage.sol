fbb09eae169b878d922a3f14ca467f0fb0c1d851
Aditya Shibu <as2397@hw.ac.uk>
2024-12-29 12:40:35 +0400

    add SPDX license identifier and define contract with a favoriteNumber variable

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,5 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Define solidity version
 pragma solidity ^0.8.0;
 
+// Define contract
 contract SimpleStorage {
-    
+    // Define unsigned integer variable
+    uint256 favoriteNumber = 5;
 }
